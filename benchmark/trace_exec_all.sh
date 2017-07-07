f8d5634eecdd2e6aac9018c4bcddd2635d1709a7
eunji <eunjicious@gmail.com>
2017-07-07 01:06:59 +0000

    trace_exec_all.sh modified

--- benchmark/trace_exec_all.sh
+++ benchmark/trace_exec_all.sh
@@ -1,6 +1,12 @@
-echo "./trace_exec.sh.sh swift"
-./trace_exec.sh.sh swift $1
-echo "./trace_exec.sh.sh rados"
-./trace_exec.sh.sh rados $1
-echo "./trace_exec.sh.sh fio"
-./trace_exec.sh.sh fio $1
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: ./.sh ms"
+  exit
+fi
+
+echo "./trace_exec.sh swift"
+./trace_exec.sh swift $1
+echo "./trace_exec.sh rados"
+./trace_exec.sh rados $1
+echo "./trace_exec.sh fio"
+./trace_exec.sh fio $1
